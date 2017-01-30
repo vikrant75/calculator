09365225c4c901d10f5deecad02586a1d55c0c48
Vikrant Gimonkar <vrgimonk@syr.edu>
2017-01-29 18:23:44 -0800

    Refactor js to use qunit

--- js/calc.js
+++ js/calc.js
@@ -6,15 +6,31 @@ var calculator = (function(){
 	var operator='';
 	var operationDone=false;
 
+	var calObject = {
+		type : null,
+		value : null,
+
+		init: function (type,value){
+			this.type = type;
+			this.value = value;
+		},
+
+		isValid: function(){
+
+			return (this.type != null && this.value != null);
+		}
+	};
+
 	//cache DOM elems
 	var $buttons = $(".btn");
+	var $display = $(".display");
 
-	$buttons.on('click',_parseInput);
+	$buttons.on('click',parseInput);
 	
 	function _displayResult(result){
-		$(".display").attr("readonly",false);
-		$(".display").val(result);
-		$(".display").attr("readonly",true);
+		$display.attr("readonly",false);
+		$display.val(result);
+		$display.attr("readonly",true);
 	}
 
 	function solve(value1,value2,operator){
@@ -44,107 +60,173 @@ var calculator = (function(){
 		currentNumber=2;
 		number1 = result == "ERROR" ? value1 : result;
 		number2 = '';
-		_displayResult(result);
 		operationDone=true;
 		return result;
 	}
 
-	function canSolve(value1,value2,operator){
+	function _canSolve(value1,value2,operator){
 
 		return ( operator !== null && !isNaN(value2) && !isNaN(value1));
 	}
 
-	function _parseInput(event){
+	function _getCalObject(elem){
+		if(elem === null)
+			return calObject;
 
-		var elem = $(event.target);
-		var value = elem.text();
-		
+		var resultObject = calObject;
+		resultObject.value = elem.text();
+
 		if(elem.hasClass("operator")){
-
-			var value1 = parseFloat(number1);
-			var value2 = parseFloat(number2);
-			
-			if(value == "%")
-			{
-				var temp;
-
-				temp = operationDone ? value1 : ((currentNumber == 1) ? value1 : value2);
-
-				if(isNaN(temp))
-					return;
-
-				solve(temp,number2,value);
-			}
-			else if(canSolve(value1,value2,operator))
-			{
-				solve(value1,value2,operator);
-			}
-			else if(!operationDone){
-				//switch input number only if prev state was 
-				//not in solve
-				currentNumber= currentNumber == 1 ? 2: 1;	
-			}
-
-			//delay assignment to operator to take into account
-			//prev calculation
-			operator=value;
+			resultObject.type = "operator";
 		}
-		if(elem.hasClass("sign")){
-
-			var temp;
-
-			temp = operationDone ? number1 : (currentNumber == 1 ? number1 : number2);
-			
-            if(isNaN(parseFloat(temp)))
-            	return;
-
-            temp = (parseFloat(temp)*(-1)).toString();
-            _displayResult(temp);
-
-            if(operationDone || currentNumber == 1)
-            	number1 = temp;
-            else
-            	number2 = temp;			
+		else if (elem.hasClass("sign")){
+			resultObject.type ="sign";
 		}
-		else if(elem.hasClass("number") || elem.hasClass("decimal")){
-			var temp ;
-
-			temp = currentNumber == 1 ? number1 : number2
-			
-			if(value != '.' || (value == '.' && temp.indexOf('.') < 0 )){
-				temp += value;
-			}
-			
-			_displayResult(temp);
-
-			if(currentNumber==1){
-				number1 = temp;
-			}else{
-				number2 = temp;
-			}
+		else if (elem.hasClass("number") || elem.hasClass("decimal")){
+			resultObject.type = "inputNumber";
 		}
 		else if(elem.hasClass("answer")){
-			
-			var value1 = parseFloat(number1);
-			var value2 = parseFloat(number2);
-			
-			if(canSolve(value1,value2,operator)){
-				solve(value1,value2,operator);
-			}
-
+			resultObject.type = "evaluate";
 		}
 		else if(elem.hasClass("clear-all")){
-			currentNumber=1;
-			number1='';
-			number2='';
-			_displayResult(0);
-			operationDone=false;
+			resultObject.type = "clear-all"
+		}
+
+		return resultObject;
+	}
+
+	function _stateOperator(oper){
+
+		var value1 = parseFloat(number1);
+		var value2 = parseFloat(number2);
+			
+		if(oper == "%")
+		{
+			var temp;
+			temp = operationDone ? value1 : ((currentNumber == 1) ? value1 : value2);
+
+			if(isNaN(temp))
+				return;
+
+			var res = solve(temp,number2,oper);
+			_displayResult(res);
+		}
+		else if(_canSolve(value1,value2,operator))
+		{
+			var res = solve(value1,value2,operator);
+			_displayResult(res);
+		}
+		else if(!operationDone){
+		 	//switch input number only if prev state was 
+			//not in solve
+			currentNumber= currentNumber == 1 ? 2: 1;	
+		}
+
+		//delay assignment to operator to take into account
+		//prev calculation
+		operator=oper;
+	}
+
+	function _stateSign(){
+		var temp;
+		temp = operationDone ? number1 : (currentNumber == 1 ? number1 : number2);
+		
+		if(isNaN(parseFloat(temp)))
+			return;
+
+        temp = (parseFloat(temp)*(-1)).toString();
+        _displayResult(temp);
+
+        if(operationDone || currentNumber == 1)
+        	number1 = temp;
+        else
+        	number2 = temp;	
+	}
+
+	function _stateInputNumber(num_val){
+		var temp ;
+		temp = currentNumber == 1 ? number1 : number2
+			
+		if(num_val != '.' || (num_val == '.' && temp.indexOf('.') < 0 )){
+
+				temp += num_val;
+			}
+			
+		_displayResult(temp);
+
+		if(currentNumber==1){
+			number1 = temp;
+		}else{
+			number2 = temp;
+			}
+	}
+
+	function _stateEvaluate(){
+		var value1 = parseFloat(number1);
+		var value2 = parseFloat(number2);
+		
+		if(_canSolve(value1,value2,operator)){
+			var res = solve(value1,value2,operator);
+			_displayResult(res);
+		}
+	}
+
+	function getInputNumber(index){
+
+		var temp =  index == 1 ? number1 : number2;
+		return parseFloat(temp);
+	}
+
+	function getOperator(){
+		return operator;
+	}
+
+	function _stateClearAll(){
+		currentNumber=1;
+		number1='';
+		number2='';
+		_displayResult(0);
+		operationDone=false;
+	}
+
+	function parseInput(event,calObject){
+
+		var inputObject = event != null ? $(event.target) : calObject;
+
+		if(event != null)
+			inputObject = _getCalObject(inputObject);
+
+		if(!inputObject.isValid()){
+
+			console.log(calObject, "input object is not a valid cal objet");
+			return;
+		}
+
+		switch(inputObject.type){
+			case "operator":
+					_stateOperator(inputObject.value);
+					break;
+			case "sign":
+					_stateSign();
+					break;
+			case "inputNumber":
+					_stateInputNumber(inputObject.value);
+					break;
+			case "evaluate":
+					_stateEvaluate();
+					break;
+			case "clear-all":
+					_stateClearAll();
+					break;
 		}
 
 	}
 
 	return {
+		parseInput:parseInput,
+		getInputNumber:getInputNumber,
+		getOperator:getOperator,
 		solve:solve,
-		canSolve:canSolve
+		calObject:calObject
 	};
 })();
--- tests/test_calculator.js
+++ tests/test_calculator.js
@@ -1,4 +1,53 @@
-QUnit.module( "calculator");
+function clearCalculator(){
+	var calObject =  calculator.calObject;
+
+	calObject.init("clear-all","AC");
+	calculator.parseInput(null,calObject);
+}
+
+function setInput(calObject,assert,expectedValue1,expectedValue2){
+
+	calculator.parseInput(null,calObject);
+
+	switch(calObject.type){
+		case "operator":
+			assert.equal(calculator.getOperator(), calObject.value, "" );
+			break;
+		case "inputNumber":
+		    if(isNaN(parseFloat(expectedValue2)) && isNaN(parseFloat(expectedValue1))){
+		    	assert.ok(isNaN(calculator.getInputNumber(1)), "" );
+		    	assert.ok(isNaN(calculator.getInputNumber(2)), "" );
+		    }
+            if(isNaN(parseFloat(expectedValue1))){
+            	assert.ok(isNaN(calculator.getInputNumber(1)), "" );
+            	assert.equal(calculator.getInputNumber(2), expectedValue2, "" );
+            }
+            else if(isNaN(parseFloat(expectedValue2))){
+            	assert.equal(calculator.getInputNumber(1), expectedValue1, "" );
+            	assert.ok(isNaN(calculator.getInputNumber(2)), "" );            	
+            }
+            else{
+            	assert.equal(calculator.getInputNumber(1), expectedValue1, "" );
+            	assert.equal(calculator.getInputNumber(2), expectedValue2, "" );
+            }
+            break;
+
+         case "evaluate":
+         		assert.equal(calculator.getInputNumber(1), expectedValue1, "" );	
+				assert.ok(isNaN(calculator.getInputNumber(2)), "" );
+			break;
+        }
+}
+
+QUnit.module( "module calculator", {
+  
+  beforeEach: function() {
+    // prepare something before each test
+    clearCalculator();
+    console.assert (isNaN(calculator.getInputNumber(1)));	
+	console.assert (isNaN(calculator.getInputNumber(2)));
+  },
+});
 
 QUnit.test( "Test function: solve", function( assert ) {
 
@@ -7,4 +56,36 @@ QUnit.test( "Test function: solve", function( assert ) {
 	assert.equal(calculator.solve(1213,21313,"-"), -20100, "test subtraction" );
 	assert.equal(calculator.solve(34,0,"/"), "ERROR", "test division" );
 	assert.equal(calculator.solve(3400,0,"%"), 34, "test percent" );
+  });
+
+QUnit.test( "test seq +,-,=,*", function( assert ) {
+
+	var calObject =  calculator.calObject;
+
+	//set input1
+	calObject.init("inputNumber","10");
+	setInput(calObject,assert,10,NaN);
+
+    //set operator
+	calObject.init("operator","+");
+	setInput(calObject,assert,NaN,NaN);
+
+	//set input2
+	calObject.init("inputNumber","10");
+	setInput(calObject,assert,10,10);
+
+	calObject.init("evaluate","=");
+	setInput(calObject,assert,20,NaN);
+
+    calObject.init("operator","-");
+    setInput(calObject,assert,20,NaN);
+
+	calObject.init("inputNumber","5");
+	setInput(calObject,assert,20,5);
+
+	calObject.init("operator","x");
+	setInput(calObject,assert,NaN,NaN);
+	assert.equal(calculator.getInputNumber(1), 15, "" );
+    assert.ok(isNaN(calculator.getInputNumber(2)), "" );
+
   });
